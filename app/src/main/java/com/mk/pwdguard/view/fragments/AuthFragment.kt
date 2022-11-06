28a755eb4886ae1299f927c223c661509dadab78
manish8621 <manimayil862@gmail.com>
2022-11-06 15:31:56 +0530

    added a animation to buttons need to add option continue without password

--- app/src/main/java/com/mk/pwdguard/view/fragments/AuthFragment.kt
+++ app/src/main/java/com/mk/pwdguard/view/fragments/AuthFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -17,7 +18,8 @@ class AuthFragment : Fragment() {
     //try two way data binding
     private lateinit var binding :FragmentAuthBinding
     private lateinit var viewModel :AuthViewModel
-
+    private val animationDuration = 100L
+    private val animationDistance = 20.0F
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -88,12 +90,47 @@ class AuthFragment : Fragment() {
                     return@setOnClickListener
                 }
                 if(viewModel.passwdMatches()) {
-                        findNavController().navigate(R.id.action_authFragment_to_homeFragment)
+                    (it as Button).text = getString(R.string.unlocked)
+                    it.also { view ->view.setBackgroundColor(requireContext().getColor(R.color.green))  }.animate().translationY(animationDistance).setDuration(animationDuration).withEndAction {
+                        it.animate().translationY(0.0F).setDuration(animationDuration).withEndAction {
+                            it.animate().translationY(animationDistance).setDuration(animationDuration).withEndAction {
+                                it.animate().translationY(0.0F).setDuration(animationDuration).withEndAction {
+                                    findNavController().navigate(R.id.action_authFragment_to_homeFragment)
+                                }.start()
+                            }.start()
+                        }.start()
+                    }.start()
+
                         Toast.makeText(context, "Unlock success", Toast.LENGTH_SHORT).show()
                 }
-                else
-                        Toast.makeText(context, "Incorrect Password !", Toast.LENGTH_SHORT).show()
+                else {
+
+                    Toast.makeText(context, "Incorrect Password !", Toast.LENGTH_SHORT).show()
+                    (it as Button).text = getString(R.string.try_again)
+                    animateError(it)
+                }
             }
+    }
+
+    private fun animateSuccess(it:View) {
+        it.also { view ->view.setBackgroundColor(requireContext().getColor(R.color.green))  }.animate().translationY(animationDistance).setDuration(animationDuration).withEndAction {
+            it.animate().translationY(0.0F).setDuration(animationDuration).withEndAction {
+                it.animate().translationY(animationDistance).setDuration(animationDuration).withEndAction {
+                    it.animate().translationY(0.0F).setDuration(animationDuration).start()
+                }.start()
+            }.start()
+        }.start()
+    }
+
+    private fun animateError(it: View) {
+
+        it.also { view ->view.setBackgroundColor(requireContext().getColor(R.color.red))  }.animate().translationX(animationDistance).setDuration(animationDuration).withEndAction {
+            it.animate().translationX(0.0F).setDuration(animationDuration).withEndAction {
+                it.animate().translationX(animationDistance).setDuration(animationDuration).withEndAction {
+                    it.also { view ->view.setBackgroundColor(requireContext().getColor(R.color.light_blue))}.animate().translationX(0.0F).setDuration(animationDuration).start()
+                }.start()
+            }.start()
+        }.start()
     }
 
 }
